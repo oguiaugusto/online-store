2d09708755791f7b6a7e39f10ebb2feba8b722e4
Guilherme <guilhaugusto05@gmail.com>
2022-04-24 22:27:44 -0300

    feat: create getCategories request

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,14 @@
+import axios from 'axios';
+
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const response = await axios.get(URL);
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+    return [];
+  }
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
