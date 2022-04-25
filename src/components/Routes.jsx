b76055012f18335fd69ee03f4b6c64bbac610f4b
Guilherme <guilhaugusto05@gmail.com>
2022-04-25 18:21:54 -0300

    feat: create carts page (empty)

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Cart from '../pages/Cart';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={ Home } />
+    <Route exact path="/carrinho" component={ Cart } />
   </Switch>
 );
 
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cart = () => (
+  <div>
+    <p
+      data-testid="shopping-cart-empty-message"
+      className="shopping-cart-empty-message"
+    >
+      Seu carrinho está vazio
+    </p>
+  </div>
+);
+
+export default Cart;
