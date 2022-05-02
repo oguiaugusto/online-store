b59518fb34ef053f0713b54e2fc16a130b272665
Guilherme <guilhaugusto05@gmail.com>
2022-05-01 23:50:33 -0300

    feat: products can be added to the cart

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-cycle */
 import React, { useContext, useEffect, useState } from 'react';
 import Loader from 'react-loader-spinner';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { MdOutlineLocalShipping } from 'react-icons/md';
 import { Context } from '../ContextAPI';
 import { getProductDetails, getProductDescription } from '../services/api';
@@ -14,8 +14,9 @@ const conditions = {
 };
 
 const Product = () => {
-  const { setQuery } = useContext(Context);
+  const { setQuery, cartProducts, addToCart } = useContext(Context);
   const { id } = useParams();
+  const history = useHistory();
 
   const [product, setProduct] = useState({});
   const [description, setDescription] = useState({});
@@ -45,12 +46,22 @@ const Product = () => {
     shipping: { free_shipping: freeShipping } = {},
     attributes,
   } = product;
+  let { available_quantity: availableQuantity = 0 } = product;
 
   const discountValue = originalPrice
     ? Math.floor(100 - (price / (originalPrice / 100))) : 0;
 
   // eslint-disable-next-line max-len
   const productCondition = `${conditions[condition] || 'Não especificado'} | ${soldQuantity} vendidos`;
+
+  const productOnCart = cartProducts.find((p) => p.id === id);
+  const productAmountOnCart = (productOnCart ? productOnCart.amount : 0) + 1;
+  // I added the + 1 because i'm assuming that the user is going to add to cart
+  // if it doesn't, nothing happens anyway
+
+  availableQuantity -= productAmountOnCart;
+  const isProductAvailable = availableQuantity >= 0;
+  // it's for the same reason up above that I put >= to 0
 
   return (
     <div className="product-page">
@@ -69,7 +80,7 @@ const Product = () => {
                 <p className="product-condition-and-sold-quantity">
                   {productCondition}
                 </p>
-                <p className="product-title">{title}</p>
+                <p className="product-title" data-testid="product-detail-name">{title}</p>
                 {
                   originalPrice && originalPrice !== price ? (
                     <div className="product-price-discount">
@@ -89,8 +100,27 @@ const Product = () => {
                       <MdOutlineLocalShipping size={ 20 } />
                       <span>Frete Grátis</span>
                     </div>) : null }
-                  <Button bgColor="#37367e" color="#ffffff" outline>Comprar</Button>
-                  <Button bgColor="#5351c2" color="#ffffff" outline>
+                  <Button
+                    bgColor="#37367e"
+                    color="#ffffff"
+                    outline
+                    disabled={ !isProductAvailable }
+                  >
+                    Comprar
+                  </Button>
+                  <Button
+                    idTest="product-detail-add-to-cart"
+                    bgColor="#5351c2"
+                    color="#ffffff"
+                    outline
+                    disabled={ !isProductAvailable }
+                    onClick={ () => {
+                      addToCart({
+                        id, title, price, thumbnail: pictures[0].url, availableQuantity,
+                      });
+                      history.push('/carrinho');
+                    } }
+                  >
                     Adicionar ao carrinho
                   </Button>
                 </div>
