4378133316a602e6c74114b38cf47a3e089c1ae4
Guilherme <guilhaugusto05@gmail.com>
2022-05-02 21:31:25 -0300

    test: fix test for cart adding limit

--- src/__tests__/requirement14.test.js
+++ src/__tests__/requirement14.test.js
@@ -5,6 +5,8 @@ import App from '../App';
 import mockedQueryResult from '../__mocks__/query';
 import mockFetch from '../__mocks__/mockFetch';
 
+jest.spyOn(window, 'scrollTo').mockImplementation(() => {});
+
 describe(`14 - Limite a quantidade de produtos adicionados ao carrinho pela quantidade disponível em estoque`, () => {
   it(`Não adiciona ao carrinho mais produtos do que o disponível em
       estoque`, async () => {
@@ -14,20 +16,23 @@ describe(`14 - Limite a quantidade de produtos adicionados ao carrinho pela quan
     await waitFor(() => expect(axios.get).toHaveBeenCalled());
     fireEvent.click(screen.getAllByTestId('category')[0]);
     await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(2));
-    fireEvent.click(screen.getAllByTestId('product-add-to-cart')[1]);
-    fireEvent.click(screen.getByTestId('shopping-cart-button'));
+    fireEvent.click(screen.getAllByTestId('product')[0]);
+    await waitFor(
+      () => expect(screen.getByTestId('product-detail-name')).toHaveTextContent(
+        mockedQueryResult.results[0].title,
+      ),
+    );
+    fireEvent.click(screen.getByTestId('product-detail-add-to-cart'));
     await waitFor(() => expect(screen.getAllByTestId('shopping-cart-product-name')));
     expect(screen.getAllByTestId('shopping-cart-product-name')[0]).toHaveTextContent(
-      mockedQueryResult.results[1].title,
+      mockedQueryResult.results[0].title,
     );
-    expect(screen.getAllByTestId('shopping-cart-product-quantity')[0]).toHaveTextContent(
-      '1',
-    );
+    expect(screen.getAllByTestId('shopping-cart-product-quantity')[0]).toHaveValue('1');
     fireEvent.click(screen.getAllByTestId('product-increase-quantity')[0]);
     fireEvent.click(screen.getAllByTestId('product-increase-quantity')[0]);
     fireEvent.click(screen.getAllByTestId('product-increase-quantity')[0]);
-    expect(screen.getAllByTestId('shopping-cart-product-quantity')[0]).toHaveTextContent(
-      mockedQueryResult.results[1].available_quantity,
-    );
+    fireEvent.click(screen.getAllByTestId('product-increase-quantity')[0]);
+    fireEvent.click(screen.getAllByTestId('product-increase-quantity')[0]);
+    expect(screen.getAllByTestId('shopping-cart-product-quantity')[0]).toHaveValue('5');
   });
 });
