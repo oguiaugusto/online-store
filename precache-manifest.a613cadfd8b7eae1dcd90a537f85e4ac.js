2100d419e35efe7386fc73affe851b782b4dcc0a
Guilherme <guilhaugusto05@gmail.com>
2022-05-03 17:04:48 -0300

    Updates

---
+++ precache-manifest.a613cadfd8b7eae1dcd90a537f85e4ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2453e00f08d4c7b27082bfab23beccbd",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "2fedcc14ee442f529c60",
+    "url": "/online-store/static/css/2.d6d5c9ca.chunk.css"
+  },
+  {
+    "revision": "000d8c5109c4bb933e4c",
+    "url": "/online-store/static/css/main.d9ccfee5.chunk.css"
+  },
+  {
+    "revision": "2fedcc14ee442f529c60",
+    "url": "/online-store/static/js/2.f10002b9.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "/online-store/static/js/2.f10002b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000d8c5109c4bb933e4c",
+    "url": "/online-store/static/js/main.13a6b464.chunk.js"
+  },
+  {
+    "revision": "ff4896c2419cfc1775c2",
+    "url": "/online-store/static/js/runtime-main.a451c0e5.js"
+  },
+  {
+    "revision": "ac3170a974eca5e025c956fb049d0eb2",
+    "url": "/online-store/static/media/sun-logo.ac3170a9.svg"
+  }
+]);
